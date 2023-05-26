f82d0116061716debc3209e54839c66af1d87340
Chiputao <420958094@qq.com>
2023-05-26 14:47:29 -0700

    updated test case for calliope

--- batteries/spindler_battery.py
+++ batteries/spindler_battery.py
@@ -1,4 +1,4 @@
-import utils.utils as utils
+from utils.utils import add_years_to_date
 
 from batteries.battery import Battery
 
@@ -9,6 +9,6 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
     
     def need_service(self) -> bool:
-        service_threshold_date = utils.add_years_to_date(self.last_service_date, 2)
+        service_threshold_date = add_years_to_date(self.last_service_date, 2)
         
         return service_threshold_date < self.current_date
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,7 @@
 def add_years_to_date(date, years_to_add:int):
     result = date.replace(year=date.year + years_to_add)
     return result
+
+def subtract_years_from_date(date, years_to_subtract:int):
+    result = date.replace(year=date.year - years_to_subtract)
+    return result
